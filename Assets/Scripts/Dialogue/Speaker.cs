09d9365cd9cc233b76a0c7fb8ee449de17554c5f
Yamigisa <ridhodwia4@gmail.com>
2023-10-02 00:44:36 +0700

    2 October 2023
    - Visual Novel Dialogue (almost completed)

---
+++ Assets/Scripts/Dialogue/Speaker.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewSpeaker", menuName = "Data/New Speaker")]
+[System.Serializable]
+
+public class Speaker : ScriptableObject
+{
+    public string speakerName;
+    public Color textColor;
+    public GameObject sprite;
+
+}
