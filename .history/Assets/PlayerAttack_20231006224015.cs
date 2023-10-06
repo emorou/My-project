8171a4fba97c8971d285c006f95f3ce888fd5a23
Yamigisa <ridhodwia4@gmail.com>
2023-10-06 23:07:40 +0700

    attack mouse

---
+++ .history/Assets/PlayerAttack_20231006224015.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private float meleeSpeed;
+    [SerializeField] private GameObject animationPivot;
+    [SerializeField] private float damage;
+
+    float timeUntilMelee = 0.5f;
+
+    private void Update()
+    {
+        if (timeUntilMelee <= 0f)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Calculate the rotation angle based on mouse position
+                Vector3 mousePosition = Input.mousePosition;
+                mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                Vector3 direction = mousePosition - transform.position;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+                // Trigger the "rotate" animation
+                anim.SetTrigger("Attack");
+
+                // Perform your attack logic here
+                // ...
+
+                timeUntilMelee = meleeSpeed;
+            }
+        }
+        else
+        {
+            timeUntilMelee -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Enemy")
+        {
+            EnemyStats enemy = col.GetComponent<EnemyStats>();
+            enemy.TakeDamage(damage);
+        }
+        else if(col.CompareTag("Prop"))
+        {
+            if(col.gameObject.TryGetComponent(out BreakableProps breakable))
+            {
+                breakable.TakeDamage(damage);
+            }
+        }   
+    }
+}
---
+++ .history/Assets/Scripts/Weapons/Weapon Base/WeaponController_20231006193310.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base script for all weapon controllers
+/// </summary>
+public class WeaponController : MonoBehaviour
+{
+    [Header("Weapon Stats")]
+    public WeaponScriptableObject weaponData;   
+
+    float currentCooldownRanged;
+    float currentCooldownMelee;
+
+    protected PlayerMovement pm;
+
+    private KnifeController knifeController;
+
+    protected virtual void Start()
+    {
+        pm = FindObjectOfType<PlayerMovement>();
+        knifeController = FindObjectOfType<KnifeController>();
+    }
+
+    protected virtual void Update()
+    {
+        currentCooldownRanged -= Time.deltaTime;
+        currentCooldownMelee -= Time.deltaTime;
+
+        if (currentCooldownRanged <= 0f && Input.GetMouseButton(1))   //Once the cooldown becomes 0, attack
+        {
+            KnifeAttack();
+            if(currentClip == 0)
+            {
+            Reload();
+            }
+        }
+        else if(currentCooldownMelee <= 0f && Input.GetMouseButton(0))   //Once the cooldown becomes 0, attack
+        {
+            MidSwordAttack();
+        }
+    }
+
+    protected virtual void MidSwordAttack()
+    {
+        currentCooldownMelee = weaponData.CooldownDurationMelee;
+    }
+
+    protected virtual void KnifeAttack()
+    {
+        currentCooldownRanged = weaponData.CooldownDurationRanged;
+    }
+
+
+}
